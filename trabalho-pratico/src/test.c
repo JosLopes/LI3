a8b36521e1c17af785a7ef53c7f6dff576143c18
voidbert <50591320+voidbert@users.noreply.github.com>
2023-10-21 23:41:29 +0100

    Fix some aspects of the dataset parser
    - Grammar definition
    - Ignore first line
    - Fix leaks

--- trabalho-pratico/src/test.c
+++ trabalho-pratico/src/test.c
@@ -15,9 +15,11 @@
  */
 
 /**
- * @file main.c
+ * @file test.c
  * @brief Contains the entry point to the program.
  */
+
+#include <inttypes.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -26,28 +28,35 @@
 #include "utils/fixed_n_delimiter_parser.h"
 #include "utils/string_utils.h"
 
+/** @brief Path to the file to be opened for this test. */
+#define TEST_FILE "testfile.txt"
+
+/** @brief Maximum number of people the test dataset can contain */
+#define TEST_MAX_PEOPLE 32
+
 typedef struct {
     char *name;
     int   age, height;
 } person_t;
 
 typedef struct {
-    int       n;
-    person_t *person_array;
-} person_parser_helper;
+    size_t   n;
+    person_t current_person;
+    person_t person_array[TEST_MAX_PEOPLE];
+} person_dataset_t;
 
 int parse_name(void *user_data, char *token, size_t ntoken) {
     (void) ntoken;
-    person_parser_helper *helper = (person_parser_helper *) user_data;
+    person_dataset_t *dataset = (person_dataset_t *) user_data;
 
-    // Copy string to another buffer
-    char *name_copy                      = string_duplicate(token);
-    helper->person_array[helper->n].name = name_copy;
+    /* Copy string to another buffer */
+    char *name_copy              = string_duplicate(token);
+    dataset->current_person.name = name_copy;
     return 0;
 }
 
 int parse_int(void *user_data, char *token, size_t ntoken) {
-    person_parser_helper *helper = (person_parser_helper *) user_data;
+    person_dataset_t *dataset = (person_dataset_t *) user_data;
 
     int value = atoi(token);
     if (value <= 0) {
@@ -56,40 +65,71 @@ int parse_int(void *user_data, char *token, size_t ntoken) {
     }
 
     if (ntoken == 1) {
-        helper->person_array[helper->n].age = value;
+        dataset->current_person.age = value;
     } else if (ntoken == 2) {
-        helper->person_array[helper->n].height = value;
-        helper->n++;
+        dataset->current_person.height = value;
     }
     return 0;
 }
 
+int add_to_reject_from_database(void *user_data, int retcode) {
+    person_dataset_t *dataset = (person_dataset_t *) user_data;
+
+    if (retcode) {
+        free(dataset->current_person.name);
+        fprintf(stderr, "Failed to parse person of name \"%s\"\n", dataset->current_person.name);
+        return 0; /* Recover and continue parsing */
+    } else {
+        if (dataset->n >= TEST_MAX_PEOPLE) {
+            free(dataset->current_person.name);
+            fputs("Filled person array!\n", stderr);
+            return 1; /* Can't continue parsing */
+        } else {
+            dataset->person_array[dataset->n] = dataset->current_person;
+            dataset->n                        = dataset->n + 1;
+            return 0;
+        }
+    }
+}
+
 int main(void) {
-    FILE *file = fopen("testfiles/testfile.txt", "r");
+    int   retcode = 0;
+    FILE *file    = fopen(TEST_FILE, "r");
+    if (!file) {
+        fputs("Failed to open file \"" TEST_FILE "\"!\n", stderr);
+        return 1;
+    }
 
-    fixed_n_delimiter_parser_iter_callback_t grammar_callbacks[3] = {parse_name,
-                                                                     parse_int,
-                                                                     parse_int};
-    fixed_n_delimiter_parser_grammar_t      *grammar =
-        fixed_n_delimiter_parser_grammar_new(';', 3, grammar_callbacks);
+    fixed_n_delimiter_parser_iter_callback_t token_grammar_callbacks[3] = {parse_name,
+                                                                           parse_int,
+                                                                           parse_int};
+    fixed_n_delimiter_parser_grammar_t      *token_grammar =
+        fixed_n_delimiter_parser_grammar_new(';', 3, token_grammar_callbacks);
+    dataset_parser_grammar_t *grammar =
+        dataset_parser_grammar_new('\n', token_grammar, add_to_reject_from_database);
 
-    person_parser_helper *helper = malloc(sizeof(person_parser_helper));
-    helper->person_array         = malloc(sizeof(person_t) * 2);
-    helper->n                    = 0;
+    person_dataset_t dataset      = {0};
+    int              parse_result = dataset_parser_parse(file, grammar, &dataset);
+    if (parse_result) {
+        fputs("Parsing failure!\n", stderr);
+        retcode = 1;
+        goto CLEANUP;
+    }
 
-    parse_dataset(file, '\n', grammar, helper);
+    for (size_t i = 0; i < dataset.n; ++i) {
+        person_t *person = dataset.person_array + i;
+        printf("Person %" PRIuPTR ": %s is %d years old and %dcm tall\n",
+               i,
+               person->name,
+               person->age,
+               person->height);
+    }
 
-    printf("PERSON1: %s, %d, %d\n",
-           helper->person_array[0].name,
-           helper->person_array[0].age,
-           helper->person_array[0].height);
-    printf("PERSON1: %s, %d, %d\n",
-           helper->person_array[1].name,
-           helper->person_array[1].age,
-           helper->person_array[1].height);
-
-    free(helper);
+CLEANUP:
     fclose(file);
-    fixed_n_delimiter_parser_grammar_free(grammar);
-    return 0;
+    dataset_parser_grammar_free(grammar);
+
+    for (size_t i = 0; i < dataset.n; ++i)
+        free(dataset.person_array[i].name);
+    return retcode;
 }
--- trabalho-pratico/src/utils/dataset_parser.c
+++ trabalho-pratico/src/utils/dataset_parser.c
@@ -26,54 +26,94 @@
 #include <stdlib.h>
 
 #include "utils/dataset_parser.h"
-#include "utils/fixed_n_delimiter_parser.h"
 #include "utils/stream_utils.h"
 
-struct stream_iter_data {
-    fixed_n_delimiter_parser_grammar_t *grammar;
-    void                               *user_data;
+/**
+ * @struct dataset_parser_grammar_t
+ * @brief  The grammar definition for a dataset parser.
+ *
+ * @var dataset_parser_grammar_t::token_grammar
+ *     @brief Grammar to use to parse single tokens with ::fixed_n_delimiter_parser_parse_string.
+ * @var dataset_parser_grammar_t::token_callback
+ *     @brief Callback called after processing each token.
+ * @var dataset_parser_grammar_t::delimiter
+ *     @brief Separator between first-order tokens (e.g.: ``'\n'`` for CSV files).
+ */
+struct dataset_parser_grammar_t {
+    fixed_n_delimiter_parser_grammar_t *token_grammar;
+    dataset_parser_token_callback       token_callback;
+    char                                delimiter;
 };
 
-stream_iter_data *stream_iter_data_new(fixed_n_delimiter_parser_grammar_t *grammar,
-                                       void                               *user_data) {
+/**
+ * @struct dataset_parser
+ * @brief A parser for datasets.
+ *
+ * @var dataset_parser_t::grammar
+ *     @brief Grammar that defines the parser.
+ *     @details Not owned by this `struct`.
+ * @var dataset_parser_t::user_data
+ *     @brief Data to be passed to callbacks in ::dataset_parser_t::grammar
+ *     @details Not owned by this `struct`.
+ * @var dataset_parser_t::first_token_passed
+ *     @brief If the first token (to be ignored) has yet been processed.
+ */
+typedef struct {
+    dataset_parser_grammar_t *grammar;
+    void                     *user_data;
+    int                       first_token_passed;
+} dataset_parser_t;
 
-    stream_iter_data *data = malloc(sizeof(stream_iter_data));
+dataset_parser_grammar_t *
+    dataset_parser_grammar_new(char                                first_order_delimiter,
+                               fixed_n_delimiter_parser_grammar_t *token_grammar,
+                               dataset_parser_token_callback       token_callback) {
 
-    data->grammar   = grammar;
-    data->user_data = user_data;
+    dataset_parser_grammar_t *grammar = malloc(sizeof(struct dataset_parser_grammar_t));
+    if (!grammar) {
+        return NULL;
+    }
 
-    return data;
+    grammar->delimiter      = first_order_delimiter;
+    grammar->token_grammar  = token_grammar;
+    grammar->token_callback = token_callback;
+
+    return grammar;
 }
 
-void stream_iter_data_free(stream_iter_data *data) {
-    free(data);
+void dataset_parser_grammar_free(dataset_parser_grammar_t *grammar) {
+    free(grammar->token_grammar);
+    free(grammar);
 }
 
 /**
  * @brief   Callback for every token parsed.
- * @details Auxiliary function for ::parse_dataset, responsible for calling
+ * @details Auxiliary function for ::dataset_parser_parse, responsible for calling
  *          ::fixed_n_delimiter_parser_parse_string with the correct data.
  *
- * @param user_data A pointer to a ::stream_iter_data.
+ * @param user_data A pointer to a ::dataset_parser_t.
  * @param token     The token to be parsed.
  */
 int __parse_stream_iter(void *user_data, char *token) {
-    stream_iter_data *data = (stream_iter_data *) user_data;
+    dataset_parser_t *parser = (dataset_parser_t *) user_data;
 
-    int retval = fixed_n_delimiter_parser_parse_string(token, data->grammar, data->user_data);
+    if (!parser->first_token_passed) {
+        parser->first_token_passed = 1;
+        return 0;
+    }
 
-    return retval;
+    int parser_ret = fixed_n_delimiter_parser_parse_string(token,
+                                                           parser->grammar->token_grammar,
+                                                           parser->user_data);
+    return parser->grammar->token_callback(parser->user_data, parser_ret);
 }
 
-int parse_dataset(FILE                               *file,
-                  char                                delimiter,
-                  fixed_n_delimiter_parser_grammar_t *grammar,
-                  void                               *user_data) {
+int dataset_parser_parse(FILE *file, dataset_parser_grammar_t *grammar, void *user_data) {
+    dataset_parser_t parser = {.grammar = grammar, .user_data = user_data, .first_token_passed = 0};
 
-    stream_iter_data *data = stream_iter_data_new(grammar, user_data);
+    int retval = stream_tokenize(file, grammar->delimiter, __parse_stream_iter, &parser);
+    if (retval == STREAM_TOKENIZE_RET_ALLOCATION_FAILURE)
+        return DATASET_PARSER_PARSE_RET_ALLOCATION_FAILURE;
 
-    stream_tokenize(file, delimiter, __parse_stream_iter, data);
-
-    stream_iter_data_free(data);
     return 0;
 }
