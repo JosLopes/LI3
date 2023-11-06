9cde570bb14b2263b3b1984c52da5736cf4d0eb2
José Matos <67171543+JoseMatos03@users.noreply.github.com>
2023-11-06 23:47:55 +0000

    Add flight_manager test and documentation

--- trabalho-pratico/include/database/flight_manager.h
+++ trabalho-pratico/include/database/flight_manager.h
@@ -14,6 +14,84 @@
  * limitations under the License.
  */
 
+/**
+ * @file    flight_manager.h
+ * @brief   Contains and manages all flights in a database.
+ * @details Usually, a flight manager won't be created by itself, but inside a ::database_t.
+ *
+ * @anchor flight_manager_examples
+ * ### Examples
+ *
+ * In the following example, a dataset is loaded into a database. The flight manager is then
+ * extracted from the database, and the program iterates over all valid flights.
+ *
+ * ```c
+ * #include <stdio.h>
+ * 
+ * #include "database/flight_manager.h"
+ * #include "dataset/dataset_loader.h"
+ * #include "types/flight.h"
+ * #include "utils/pool.h"
+ * 
+ * int iter_callback(void *user_data, flight_t *flight) {
+ *     (void) user_data;
+ * 
+ *     size_t      id          = flight_get_id(flight);
+ *     const char *airline     = flight_get_const_airline(flight);
+ *     const char *passport    = flight_get_const_plane_model(flight);
+ *     int         total_seats = flight_get_total_seats(flight);
+ * 
+ *     char origin[AIRPORT_CODE_SPRINTF_MIN_BUFFER_SIZE];
+ *     airport_code_sprintf(origin, flight_get_origin(flight));
+ * 
+ *     char destination[AIRPORT_CODE_SPRINTF_MIN_BUFFER_SIZE];
+ *     airport_code_sprintf(destination, flight_get_destination(flight));
+ * 
+ *     char schedule_departure_date[DATE_SPRINTF_MIN_BUFFER_SIZE];
+ *     date_sprintf(schedule_departure_date, flight_get_schedule_departure_date(flight));
+ * 
+ *     char schedule_arrival_date[DATE_SPRINTF_MIN_BUFFER_SIZE];
+ *     date_sprintf(schedule_arrival_date, flight_get_schedule_arrival_date(flight));
+ * 
+ *     char real_departure_date[DATE_SPRINTF_MIN_BUFFER_SIZE];
+ *     date_sprintf(real_departure_date, flight_get_real_departure_date(flight));
+ * 
+ *     printf("--- FLIGHT ---\nid: %zu\nairline: %s\nplane_model: %s\ntotal_seats: %d\norigin: %s"
+ *            "\ndestination: %s\nschedule_departure_date: %s\nschedule_arrival_date: %s\n"
+ *            "real_departure_date: %s\n\n",
+ *            id,
+ *            airline,
+ *            passport,
+ *            total_seats,
+ *            origin,
+ *            destination,
+ *            schedule_departure_date,
+ *            schedule_arrival_date,
+ *            real_departure_date);
+ * 
+ *     return 0; // You can return a value other than 0 to order iteration to stop
+ * }
+ * 
+ * int main() {
+ *     database_t *database = database_create();
+ *     if (!database) {
+ *         fprintf(stderr, "Failed to allocate database!");
+ *         return 1;
+ *     }
+ * 
+ *     if (dataset_loader_load(database, "/path/to/dataset/directory")) {
+ *         fputs("Failed to open dataset to be parsed.\n", stderr);
+ *         return 1;
+ *     }
+ * 
+ *     flight_manager_iter(database_get_flights(database), iter_callback, NULL);
+ * 
+ *     database_free(database);
+ *     return 0;
+ * }
+ * ```
+ */
+
 #ifndef FLIGHT_MANAGER_H
 #define FLIGHT_MANAGER_H
 
