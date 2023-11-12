b9f2ef3cee678150a8afe9e0a968d4e3d6c7ff8b
voidbert <50591320+voidbert@users.noreply.github.com>
2023-11-12 19:10:05 +0000

    Black slate test program (again)

--- trabalho-pratico/src/test.c
+++ trabalho-pratico/src/test.c
@@ -16,13 +16,10 @@
 
 /**
  * @file test.c
- * @brief Contains the entry point to the program.
+ * @brief Contains the entry point to the test program.
  */
 
 #include <stdio.h>
-#include <stdlib.h>
-
-#include "queries/query_parser.h"
 
 /**
  * @brief The entry point to the test program.
@@ -31,35 +28,6 @@
  * @retval 1 Insuccess
  */
 int main(void) {
-    const char *queries[5] = {"88F Hello world",
-                              "1 \"Hello, world\" test      123 \"last ends in quotes\"",
-                              "1F \"multiple   spaces\"",
-                              "3 \"unknown query\" \"number\"",
-                              "3F \"unknown formatted\" \"query\""};
-
-    query_type_list_t *query_list = query_type_list_create();
-    if (!query_list) {
-        fprintf(stderr, "Failed to allocate query definitions!\n");
-        return 1;
-    }
-
-    GPtrArray *aux = g_ptr_array_new();
-
-    for (size_t i = 0; i < 5; ++i) {
-        query_instance_t *query = query_instance_create();
-
-        int result = query_parser_parse_string_const(query, queries[i], query_list, aux);
-        if (result)
-            fprintf(stderr, "Failed to parse query: %s\n", queries[i]);
-        else if (query_instance_get_formatted(query))
-            printf("Query's output must be formatted!\n");
-        else
-            printf("Query's output should not be formatted!\n");
-
-        query_instance_free(query, query_list);
-    }
-
-    query_type_list_free(query_list);
-    g_ptr_array_free(aux, TRUE);
+    printf("Hello, world!");
     return 0;
 }
