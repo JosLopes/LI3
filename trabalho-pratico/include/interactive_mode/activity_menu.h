07edd134ee1310dbc1e331b53f4a37a57ed2c3ac
José Lopes <jlopesum004@gmail.com>
2023-12-02 18:18:06 +0000

    Added docs with an example

--- trabalho-pratico/include/interactive_mode/activity_menu.h
+++ trabalho-pratico/include/interactive_mode/activity_menu.h
@@ -14,6 +14,30 @@
  * limitations under the License.
  */
 
+/**
+ * @file  activity_menu.h
+ * @brief A menu that takes a set of options.
+ *
+ * @anchor activity_menu_examples
+ * ### Examples
+ *
+ * Creating a menu is as simple as calling ::activity_menu_run. Just provide a title, a set of
+ * options you want displayed on the menu, and the number of options in that set.
+ *
+ * The menu will look like the following on screen:
+ *
+ * ```text
+ *    TITLE
+ * +------------+
+ * | Option_0   |
+ * | Option_1   |
+ * | Option_2   |
+ * | (...)      |
+ * +------------+
+ * ```
+ * If the window is not big enough, the options that wont fit the screen can still be accessed by
+ * the user, as this menu supports scrolling by using `KEY_DOWN` when at the bottom the menu.
+ */
 #ifndef ACTIVITY_MENU_H
 #define ACTIVITY_MENU_H
 
@@ -21,6 +45,19 @@
 
 #include "interactive_mode/activity.h"
 
-int activity_menu_run(const char *title, const char **screen_options, size_t number_of_options); 
+/**
+ * @brief Runs a TUI activity for a menu.
+ *
+ * @param title             The title of the menu that will be shown on the screen.
+ * @param screen_options    The set of options available to a user in the menu.
+ * @param number_of_options The number of options (strings) in @p screen_options.
+ *
+ * @return The index of the user selected option. `-1` in case the user escaped the menu by using
+ *         `\x1b` (Escape key), or if an error occurred.
+ *
+ * #### Examples
+ * See [the header file's documentation](@ref activity_menu_examples).
+ */
+int activity_menu_run(const char *title, const char **screen_options, size_t number_of_options);
 
 #endif
