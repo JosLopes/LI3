a4289d961a8259edbd273355e5ac7fff7b445eff
José Lopes <jlopesum004@gmail.com>
2023-12-17 22:51:20 +0000

    Update header docs

--- trabalho-pratico/include/queries/q05.h
+++ trabalho-pratico/include/queries/q05.h
@@ -15,9 +15,20 @@
  */
 
 /**
- * @file   q05.h
- * @brief   A placeholder query.
- * @details TODO - Remove when all queries are done
+ * @file  q05.h
+ * @brief A query to present all of the flights scheduled to departure, given an origin airport and
+ *        a time frame.
+ *
+ * ### Examples
+ *
+ * ```text
+ * 5 LIS "2021/01/01 00:00:00" "2022/12/31 23:59:59"
+ * 5 LIS "2023/01/01 00:00:00" "2023/05/31 23:59:59"
+ * 5 AMS "2021/01/01 00:00:00" "2024/01/01 23:59:59"
+ * 5F LIS "2021/01/01 00:00:00" "2022/12/31 23:59:59"
+ * 5F LIS "2023/01/01 00:00:00" "2023/05/31 23:59:59"
+ * 5F AMS "2021/01/01 00:00:00" "2024/01/01 23:59:59"
+ * ```
  */
 
 #ifndef Q05_H
@@ -26,7 +37,7 @@
 #include "queries/query_type.h"
 
 /**
- * @brief   Initializes the definition of this placeholder query.
+ * @brief   Initializes the definition of the fifth query.
  * @details This is done automatically in ::query_type_list_init.
  * @return  A pointer to a `malloc`-allocated ::query_type_t on success, or `NULL` on failure.
  */
