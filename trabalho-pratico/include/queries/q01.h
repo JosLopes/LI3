77e1c71e16538be572460133f68dfa7522cedd22
voidbert <50591320+voidbert@users.noreply.github.com>
2024-01-21 16:15:51 +0000

    Cleanup of query 1

--- trabalho-pratico/include/queries/q01.h
+++ trabalho-pratico/include/queries/q01.h
@@ -15,8 +15,8 @@
  */
 
 /**
- * @file    q01.h
- * @brief   Implementation of query 1.
+ * @file  q01.h
+ * @brief A query to provide information about an entity in the dataset.
  *
  * ### Examples
  *
@@ -66,9 +66,10 @@
 #include "queries/query_type.h"
 
 /**
- * @brief   Initializes the definition of query 1.
- * @details This is done automatically in ::query_type_list_create.
- * @return  A pointer to a `malloc`-allocated ::query_type_t on success, or `NULL` on failure.
+ * @brief   Initializes the definition of queries of type 1.
+ * @details This is done automatically in [query_type_list](@ref query_type_list.c).
+ * @return  On success, a pointer to a ::query_type_t that must be deleted with ::query_type_free,
+ *          or `NULL` allocation on failure.
  */
 query_type_t *q01_create(void);
 
--- trabalho-pratico/src/queries/q01.c
+++ trabalho-pratico/src/queries/q01.c
@@ -19,29 +19,27 @@
  * @brief Implementation of methods in include/queries/q01.h
  */
 
-#include <stdio.h>
 #include <stdlib.h>
 
 #include "queries/q01.h"
 #include "queries/query_instance.h"
-#include "utils/int_utils.h"
-#include "utils/single_pool_id_linked_list.h"
 
-/** @brief Type of the enitity queried by a query 1 */
+/** @brief Type of the entity queried by a query 1. */
 typedef enum {
-    ID_ENTITY_USER,       /**< @brief The queried entity is a user */
-    ID_ENTITY_FLIGHT,     /**< @brief The queried entity is a flight */
-    ID_ENTITY_RESERVATION /**< @brief The queried entity is a reservation */
+    ID_ENTITY_USER,       /**< @brief The queried entity is a user. */
+    ID_ENTITY_FLIGHT,     /**< @brief The queried entity is a flight. */
+    ID_ENTITY_RESERVATION /**< @brief The queried entity is a reservation. */
 } q01_id_entity_t;
 
 /**
  * @struct q01_parsed_arguments_t
- * @brief  Data needed for the execution of a query of type 1.
+ * @brief  Parsed arguments of a query of type 1.
  *
- * @var parse_arguments_helper::id_entity
- *     @brief Stores the type of the entity of an identifier.
- * @var parse_arguments_helper::parsed_id
- *     @brief A pointer to an id, of type `char *` or `uint64_t *`.
+ * @var q01_parsed_arguments_t::id_entity
+ *     @brief The type of the entity q01_parsed_arguments_t::parsed_id refers to.
+ * @var q01_parsed_arguments_t::parsed_id
+ *     @brief A pointer to a string (user identifier), or a ::flight_id_t / ::reservation_id_t
+ *            encoded as a pointer.
  */
 typedef struct {
     q01_id_entity_t id_entity;
@@ -50,146 +48,129 @@ typedef struct {
 
 /**
  * @brief   Parses the arguments of a query of type 1.
- * @details Asserts there's only one string argument, that is stored.
+ * @details Asserts there's only one argument, the identifier of a user, flight or reservation.
  *
  * @param argc Number of arguments.
  * @param argv Values of the arguments.
  *
- * @return `NULL` for invalid arguments, a copy of the only @p argv and its identifier on success.
+ * @return `NULL` for invalid arguments (or allocation failure), a pointer to a valid
+ *         ::q01_parsed_arguments_t on success.
  */
 void *__q01_parse_arguments(size_t argc, char *const argv[argc]) {
     if (argc != 1)
         return NULL;
 
-    /* TODO - fix leaks */
-
-    flight_id_t             parsed_flight_id_aux;
-    reservation_id_t        parsed_reservation_id_aux;
-    q01_parsed_arguments_t *parsed_argument = malloc(sizeof(q01_parsed_arguments_t));
+    flight_id_t                   parsed_flight_id;
+    reservation_id_t              parsed_reservation_id;
+    q01_parsed_arguments_t *const parsed_argument = malloc(sizeof(q01_parsed_arguments_t));
     if (!parsed_argument)
         return NULL;
 
-    if (!flight_id_from_string(&parsed_flight_id_aux, *argv)) {
-        flight_id_t *parsed_id = malloc(sizeof(flight_id_t));
-        if (!parsed_id)
-            return NULL;
-
-        *parsed_id                 = parsed_flight_id_aux;
+    if (!flight_id_from_string(&parsed_flight_id, *argv)) {
         parsed_argument->id_entity = ID_ENTITY_FLIGHT;
-        parsed_argument->parsed_id = parsed_id;
-    } else if (!reservation_id_from_string(&parsed_reservation_id_aux, *argv)) {
-        reservation_id_t *parsed_id = malloc(sizeof(reservation_id_t));
-        if (!parsed_id)
-            return NULL;
-
-        *parsed_id                 = parsed_reservation_id_aux;
+        parsed_argument->parsed_id = (void *) (size_t) parsed_flight_id;
+    } else if (!reservation_id_from_string(&parsed_reservation_id, *argv)) {
         parsed_argument->id_entity = ID_ENTITY_RESERVATION;
-        parsed_argument->parsed_id = parsed_id;
+        parsed_argument->parsed_id = (void *) (size_t) parsed_reservation_id;
     } else {
-        char *parsed_id = strdup(*argv);
-        if (!parsed_id)
+        parsed_argument->parsed_id = strdup(*argv);
+        if (!parsed_argument->parsed_id) {
+            free(parsed_argument);
             return NULL;
-
+        }
         parsed_argument->id_entity = ID_ENTITY_USER;
-        parsed_argument->parsed_id = parsed_id;
     }
 
     return parsed_argument;
 }
 
+/**
+ * @brief  Creates a deep clone of the value returned by ::__q01_parse_arguments.
+ * @param  args_data Non-`NULL` value returned by ::__q01_parse_arguments (a pointer to a
+ *                   ::q01_parsed_arguments_t).
+ * @return A deep copy of @p args_data.
+ */
 void *__q01_clone_arguments(const void *args_data) {
-    const q01_parsed_arguments_t *args  = args_data;
-    q01_parsed_arguments_t       *clone = malloc(sizeof(q01_parsed_arguments_t));
+    const q01_parsed_arguments_t *const args  = args_data;
+    q01_parsed_arguments_t *const       clone = malloc(sizeof(q01_parsed_arguments_t));
     if (!clone)
         return NULL;
 
     clone->id_entity = args->id_entity;
     switch (args->id_entity) {
         case ID_ENTITY_FLIGHT:
-            clone->parsed_id = malloc(sizeof(sizeof(flight_id_t)));
-            memcpy(clone->parsed_id, args->parsed_id, sizeof(flight_id_t));
-            break;
         case ID_ENTITY_RESERVATION:
-            clone->parsed_id = malloc(sizeof(sizeof(reservation_id_t)));
-            memcpy(clone->parsed_id, args->parsed_id, sizeof(reservation_id_t));
+            clone->parsed_id = args->parsed_id;
             break;
         case ID_ENTITY_USER:
             clone->parsed_id = strdup(args->parsed_id);
+            if (!clone->parsed_id) {
+                free(clone);
+                return NULL;
+            }
             break;
-        default:
-            clone->parsed_id = NULL; /* unreachable */
-            break;
-    }
-
-    if (!clone->parsed_id) {
-        free(clone);
-        return NULL;
     }
     return clone;
 }
 
 /**
  * @brief Frees data generated by ::__q01_parse_arguments.
- * @param argument_data Data generated by ::__q01_parse_arguments.
+ * @param args_data Data generated by ::__q01_parse_arguments.
  */
-void __q01_free_query_instance_argument_data(void *argument_data) {
-    free(((q01_parsed_arguments_t *) argument_data)->parsed_id);
-    free(argument_data);
+void __q01_free_arguments(void *args_data) {
+    q01_parsed_arguments_t *const args = args_data;
+    if (args->id_entity == ID_ENTITY_USER)
+        free(args->parsed_id);
+    free(args);
 }
 
 /**
- * @brief   Calculates the total spent by a ::user_t.
- * @details The total spent is the sum of the total price of all the reservations the user is
- *          related to.
+ * @brief   Calculates the total money spent by a ::user_t.
  *
  * @param list    List to get the reservation identifiers from.
  * @param manager Manager to get the reservations from.
  *
- * @return The total spent by a ::user_t.
+ * @return The sum of the total price of all the reservations a user booked.
  */
 double __q01_calculate_user_total_spent(const single_pool_id_linked_list_t *list,
                                         const reservation_manager_t        *manager) {
     double total_spent = 0.0;
-
     while (list) {
-        const reservation_t *reservation =
+        const reservation_t *const reservation =
             reservation_manager_get_by_id(manager, single_pool_id_linked_list_get_value(list));
         total_spent += reservation_calculate_price(reservation);
         list = single_pool_id_linked_list_get_next(list);
     }
-
     return total_spent;
 }
 
 /**
  * @brief Executes a query of type 1, when it refers to a ::user_t.
  *
- * @param database Database do get the users and reservations from.
- * @param id       Identifier to find the user and its data.
+ * @param database Database do get users and reservations from.
+ * @param id       Identifier of the user to be found.
  * @param output   Where to write the query's output to.
- *
- * @retval 0 Never fails.
  */
-int __q01_execute_user_entity(const database_t *database, const char *id, query_writer_t *output) {
-
-    const user_manager_t        *user_manager        = database_get_users(database);
-    const reservation_manager_t *reservation_manager = database_get_reservations(database);
-    const user_t                *user                = user_manager_get_by_id(user_manager, id);
+void __q01_execute_user_entity(const database_t *database, const char *id, query_writer_t *output) {
+    const user_manager_t *const        user_manager        = database_get_users(database);
+    const reservation_manager_t *const reservation_manager = database_get_reservations(database);
+    const user_t *const                user = user_manager_get_by_id(user_manager, id);
     if (!user || user_get_account_status(user) == ACCOUNT_STATUS_INACTIVE)
-        return 0;
+        return;
 
-    const single_pool_id_linked_list_t *reservation_list =
-        user_manager_get_reservations_by_id(user_manager, id);
-    size_t number_of_flights =
+    const size_t number_of_flights =
         single_pool_id_linked_list_length(user_manager_get_flights_by_id(user_manager, id));
-    size_t number_of_reservations = single_pool_id_linked_list_length(reservation_list);
 
-    double total_spent = __q01_calculate_user_total_spent(reservation_list, reservation_manager);
+    const single_pool_id_linked_list_t *const reservation_list =
+        user_manager_get_reservations_by_id(user_manager, id);
+    const size_t number_of_reservations = single_pool_id_linked_list_length(reservation_list);
+    const double total_spent =
+        __q01_calculate_user_total_spent(reservation_list, reservation_manager);
 
     char sex[SEX_SPRINTF_MIN_BUFFER_SIZE];
     sex_sprintf(sex, user_get_sex(user));
 
-    int32_t age = user_calculate_age(user);
+    const int32_t age = user_calculate_age(user);
 
     char country_code[COUNTRY_CODE_SPRINTF_MIN_BUFFER_SIZE];
     country_code_sprintf(country_code, user_get_country_code(user));
@@ -203,42 +184,37 @@ int __q01_execute_user_entity(const database_t *database, const char *id, query_
     query_writer_write_new_field(output, "number_of_flights", "%zu", number_of_flights);
     query_writer_write_new_field(output, "number_of_reservations", "%zu", number_of_reservations);
     query_writer_write_new_field(output, "total_spent", "%.3lf", total_spent);
-
-    return 0;
 }
 
 /**
  * @brief Executes a query of type 1, when it refers to a ::reservation_t.
  *
- * @param database Database do get the users and reservations from.
- * @param id       Identifier to find the reservation and its data.
+ * @param database Database do get reservations from.
+ * @param id       Identifier of the reservation to be found.
  * @param output   Where to write the query's output to.
- *
- * @retval 0 Always successful.
  */
-int __q01_execute_reservation_entity(const database_t *database,
-                                     reservation_id_t  id,
-                                     query_writer_t   *output) {
-
-    const reservation_manager_t *reservation_manager = database_get_reservations(database);
-    const reservation_t *reservation = reservation_manager_get_by_id(reservation_manager, id);
+void __q01_execute_reservation_entity(const database_t *database,
+                                      reservation_id_t  id,
+                                      query_writer_t   *output) {
+    const reservation_t *const reservation =
+        reservation_manager_get_by_id(database_get_reservations(database), id);
     if (!reservation)
-        return 0;
+        return;
 
-    date_t begin_date = reservation_get_begin_date(reservation);
-    char   begin_date_str[DATE_SPRINTF_MIN_BUFFER_SIZE];
+    const date_t begin_date = reservation_get_begin_date(reservation);
+    char         begin_date_str[DATE_SPRINTF_MIN_BUFFER_SIZE];
     date_sprintf(begin_date_str, begin_date);
 
-    date_t end_date = reservation_get_end_date(reservation);
-    char   end_date_str[DATE_SPRINTF_MIN_BUFFER_SIZE];
+    const date_t end_date = reservation_get_end_date(reservation);
+    char         end_date_str[DATE_SPRINTF_MIN_BUFFER_SIZE];
     date_sprintf(end_date_str, end_date);
 
-    includes_breakfast_t includes_breakfast = reservation_get_includes_breakfast(reservation);
-    char                 includes_breakfast_str[INCLUDES_BREAKFAST_SPRINTF_MIN_BUFFER_SIZE];
+    const includes_breakfast_t includes_breakfast = reservation_get_includes_breakfast(reservation);
+    char                       includes_breakfast_str[INCLUDES_BREAKFAST_SPRINTF_MIN_BUFFER_SIZE];
     includes_breakfast_sprintf(includes_breakfast_str, includes_breakfast);
 
-    int64_t nights      = date_diff(end_date, begin_date);
-    double  total_price = reservation_calculate_price(reservation);
+    const int64_t nights      = date_diff(end_date, begin_date);
+    const double  total_price = reservation_calculate_price(reservation);
 
     char hotel_id_str[HOTEL_ID_SPRINTF_MIN_BUFFER_SIZE];
     hotel_id_sprintf(hotel_id_str, reservation_get_hotel_id(reservation));
@@ -258,27 +234,23 @@ int __q01_execute_reservation_entity(const database_t *database,
     query_writer_write_new_field(output, "includes_breakfast", "%s", includes_breakfast_str);
     query_writer_write_new_field(output, "nights", "%" PRIi64, nights);
     query_writer_write_new_field(output, "total_price", "%.3lf", total_price);
-
-    return 0;
 }
 
 /**
  * @brief Executes a query of type 1, when it refers to a ::flight_t.
  *
  * @param database Database do get the flights from.
- * @param id       Id to find the flight.
+ * @param id       Identifier of the flight to be found.
  * @param output   Where to write the query's output to.
- *
- * @retval 0 Always successful.
  */
-int __q01_execute_flight_entity(const database_t *database,
-                                flight_id_t       id,
-                                query_writer_t   *output) {
+void __q01_execute_flight_entity(const database_t *database,
+                                 flight_id_t       id,
+                                 query_writer_t   *output) {
 
-    const flight_manager_t *flight_manager = database_get_flights(database);
-    const flight_t         *flight         = flight_manager_get_by_id(flight_manager, id);
+    const flight_manager_t *const flight_manager = database_get_flights(database);
+    const flight_t *const         flight         = flight_manager_get_by_id(flight_manager, id);
     if (!flight)
-        return 0;
+        return;
 
     char origin_airport[AIRPORT_CODE_SPRINTF_MIN_BUFFER_SIZE];
     airport_code_sprintf(origin_airport, flight_get_origin(flight));
@@ -286,14 +258,14 @@ int __q01_execute_flight_entity(const database_t *database,
     char destination_airport[AIRPORT_CODE_SPRINTF_MIN_BUFFER_SIZE];
     airport_code_sprintf(destination_airport, flight_get_destination(flight));
 
-    date_and_time_t schedule_departure_date = flight_get_schedule_departure_date(flight);
-    char            scheduled_departure_str[DATE_AND_TIME_SPRINTF_MIN_BUFFER_SIZE];
+    const date_and_time_t schedule_departure_date = flight_get_schedule_departure_date(flight);
+    char                  scheduled_departure_str[DATE_AND_TIME_SPRINTF_MIN_BUFFER_SIZE];
     date_and_time_sprintf(scheduled_departure_str, schedule_departure_date);
 
     char scheduled_arrival_str[DATE_AND_TIME_SPRINTF_MIN_BUFFER_SIZE];
     date_and_time_sprintf(scheduled_arrival_str, flight_get_schedule_arrival_date(flight));
 
-    int64_t delay =
+    const int64_t delay =
         date_and_time_diff(flight_get_real_departure_date(flight), schedule_departure_date);
 
     query_writer_write_new_object(output);
@@ -308,20 +280,17 @@ int __q01_execute_flight_entity(const database_t *database,
                                  "%" PRIu16,
                                  flight_get_number_of_passengers(flight));
     query_writer_write_new_field(output, "delay", "%" PRIi64, delay);
-
-    return 0;
 }
 
 /**
  * @brief Executes a query of type 1.
  *
- * @param database   Database where to get users/reservations/flights from
+ * @param database   Database where to get users / reservations / flights from
  * @param statistics `NULL`, as this query does not generate statistical data.
  * @param instance   Query instance to be executed.
  * @param output     Where to write the query's output to.
  *
- * @retval 0 On success.
- * @retval 1 On failure.
+ * @retval 0 Always successful.
  */
 int __q01_execute(const database_t       *database,
                   const void             *statistics,
@@ -329,26 +298,28 @@ int __q01_execute(const database_t       *database,
                   query_writer_t         *output) {
     (void) statistics;
 
-    const q01_parsed_arguments_t *arguments = query_instance_get_argument_data(instance);
-    const void                   *id        = arguments->parsed_id;
+    const q01_parsed_arguments_t *const arguments = query_instance_get_argument_data(instance);
+    const void *const                   id        = arguments->parsed_id;
 
     switch (arguments->id_entity) {
         case ID_ENTITY_USER:
-            return __q01_execute_user_entity(database, (const char *) id, output);
+            __q01_execute_user_entity(database, (const char *) id, output);
+            break;
         case ID_ENTITY_RESERVATION:
-            return __q01_execute_reservation_entity(database, *(reservation_id_t *) id, output);
+            __q01_execute_reservation_entity(database, (reservation_id_t) (size_t) id, output);
+            break;
         case ID_ENTITY_FLIGHT:
-            return __q01_execute_flight_entity(database, *(flight_id_t *) id, output);
-        default:
-            return 1; /* unreachable */
+            __q01_execute_flight_entity(database, (flight_id_t) (size_t) id, output);
+            break;
     }
+    return 0;
 }
 
 query_type_t *q01_create(void) {
     return query_type_create(1,
                              __q01_parse_arguments,
                              __q01_clone_arguments,
-                             __q01_free_query_instance_argument_data,
+                             __q01_free_arguments,
                              NULL,
                              NULL,
                              __q01_execute);
