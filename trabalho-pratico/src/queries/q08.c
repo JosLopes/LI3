ca47a1dbda128496b718b7b23c4364c95fbe473b
voidbert <50591320+voidbert@users.noreply.github.com>
2024-01-22 17:18:25 +0000

    Query 8 cleanup and slight optimization

--- trabalho-pratico/src/queries/q08.c
+++ trabalho-pratico/src/queries/q08.c
@@ -19,7 +19,6 @@
  * @brief Implementation of methods in include/queries/q08.h
  */
 
-#include <glib.h>
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -28,7 +27,7 @@
 
 /**
  * @struct q08_parsed_arguments_t
- * @brief  Data needed for the execution of a query of type 8.
+ * @brief  Parsed arguments of a query of type 8.
  *
  * @var q08_parsed_arguments_t::hotel_id
  *     @brief Hotel identifier to filter by.
@@ -45,36 +44,42 @@ typedef struct {
 
 /**
  * @brief   Parses arguments for query 8.
- * @details Asserts that there's three arguments, an hotel identifier, and two dates.
+ * @details Asserts that there's three arguments, an hotel identifier and two dates.
  *
  * @param argc Number of arguments.
  * @param argv Values of the arguments.
  *
- * @return `NULL` on failure, a pointer to a `q08_airport_data_t` otherwise.
+ * @return `NULL` on parsing or allocation failure, a pointer to a ::q08_parsed_arguments_t
+ *         otherwise.
  */
 void *__q08_parse_arguments(size_t argc, char *const argv[argc]) {
     if (argc != 3)
         return NULL;
 
-    q08_parsed_arguments_t *parsed_arguments = malloc(sizeof(q08_parsed_arguments_t));
+    q08_parsed_arguments_t *const parsed_arguments = malloc(sizeof(q08_parsed_arguments_t));
     if (!parsed_arguments)
         return NULL;
 
-    int hotel_retval      = hotel_id_from_string(&parsed_arguments->hotel_id, argv[0]);
-    int begin_date_retval = date_from_string(&parsed_arguments->begin_date, argv[1]);
-    int end_date_retval   = date_from_string(&parsed_arguments->end_date, argv[2]);
+    const int hotel_retval      = hotel_id_from_string(&parsed_arguments->hotel_id, argv[0]);
+    const int begin_date_retval = date_from_string(&parsed_arguments->begin_date, argv[1]);
+    const int end_date_retval   = date_from_string(&parsed_arguments->end_date, argv[2]);
 
     if (hotel_retval || begin_date_retval || end_date_retval) {
         free(parsed_arguments);
         return NULL;
     }
-
     return parsed_arguments;
 }
 
+/**
+ * @brief  Creates a deep clone of the value returned by ::__q08_parse_arguments.
+ * @param  args_data Non-`NULL` value returned by ::__q08_parse_arguments (a pointer to a
+ *                   ::q08_parsed_arguments_t).
+ * @return A deep copy of @p args_data.
+ */
 void *__q08_clone_arguments(const void *args_data) {
-    const q08_parsed_arguments_t *args  = args_data;
-    q08_parsed_arguments_t       *clone = malloc(sizeof(q08_parsed_arguments_t));
+    const q08_parsed_arguments_t *const args  = args_data;
+    q08_parsed_arguments_t *const       clone = malloc(sizeof(q08_parsed_arguments_t));
     if (!clone)
         return NULL;
 
@@ -83,63 +88,58 @@ void *__q08_clone_arguments(const void *args_data) {
 }
 
 /**
- * @struct q08_foreach_reservation_data_t
- * @brief  Data needed while iterating over reservations.
+ * @struct q08_statistical_data_t
+ * @brief  Association between query instances and hotel revenue prices.
  *
- * @var q08_foreach_reservation_data_t::filter_data
- *     @brief Array of pointers to ::q08_parsed_arguments_t.
- * @var q08_foreach_reservation_data_t::hotel_revenue
- *     @brief Associations between ::q08_parsed_arguments_t and it's hotel revenue.
+ * @var q08_statistical_data_t::n
+ *     @brief Number of query instances (same as ::q08_statistical_data_t::filters and
+ *            ::q08_statistical_data_t::revenues).
+ * @var q08_statistical_data_t::filters
+ *     @brief Query instances that need to be answered.
+ * @var q08_statistical_data_t::revenues
+ *     @brief Revenues of hotels (associated to ::q08_statistical_data_t::filters).
  */
 typedef struct {
-    GPtrArray  *filter_data;
-    GHashTable *hotel_revenue;
-} q08_foreach_reservation_data_t;
+    size_t                  n;
+    q08_parsed_arguments_t *filters;
+    uint64_t               *revenues;
+} q08_statistical_data_t;
 
 /**
  * @brief   A method called for each reservation, to calculate the revenue of a hotel.
  * @details An auxiliary method for ::__q08_generate_statistics.
  *
- * @param user_data   A pointer to a ::q08_foreach_reservation_data_t.
+ * @param user_data   A pointer to a ::q08_statistical_data_t.
  * @param reservation Reservation being processed.
  *
  * @retval 0 Always successful.
  */
 int __q08_generate_statistics_foreach_reservation(void                *user_data,
                                                   const reservation_t *reservation) {
-    q08_foreach_reservation_data_t *foreach_data = (q08_foreach_reservation_data_t *) user_data;
+    q08_statistical_data_t *const stats    = user_data;
+    const hotel_id_t              hotel_id = reservation_get_hotel_id(reservation);
 
-    hotel_id_t hotel_id = reservation_get_hotel_id(reservation);
-
-    for (size_t i = 0; i < foreach_data->filter_data->len; i++) {
-        const q08_parsed_arguments_t *args = g_ptr_array_index(foreach_data->filter_data, i);
+    for (size_t i = 0; i < stats->n; i++) {
+        const q08_parsed_arguments_t *const args = &stats->filters[i];
 
         if (hotel_id == args->hotel_id) {
-            uint16_t price_per_night = reservation_get_price_per_night(reservation);
-
-            date_t reservation_begin = reservation_get_begin_date(reservation),
-                   reservation_end   = reservation_get_end_date(reservation);
+            const uint16_t price_per_night   = reservation_get_price_per_night(reservation);
+            const date_t   reservation_begin = reservation_get_begin_date(reservation);
+            date_t         reservation_end   = reservation_get_end_date(reservation);
 
             /* Reservations don't make money on their last day */
             date_set_day(&reservation_end, date_get_day(reservation_end) - 1);
-
             if (date_diff(args->begin_date, reservation_end) > 0 ||
                 date_diff(reservation_begin, args->end_date) > 0)
                 continue;
 
-            date_t range_begin = date_diff(reservation_begin, args->begin_date) < 0
-                                     ? args->begin_date
-                                     : reservation_begin;
-            date_t range_end =
+            const date_t range_begin = date_diff(reservation_begin, args->begin_date) < 0
+                                           ? args->begin_date
+                                           : reservation_begin;
+            const date_t range_end =
                 date_diff(reservation_end, args->end_date) < 0 ? reservation_end : args->end_date;
 
-            uint64_t reservation_revenue =
-                price_per_night * (date_diff(range_end, range_begin) + 1);
-            uint64_t revenue =
-                GPOINTER_TO_UINT(g_hash_table_lookup(foreach_data->hotel_revenue, args));
-            g_hash_table_insert(foreach_data->hotel_revenue,
-                                (q08_parsed_arguments_t *) args,
-                                GUINT_TO_POINTER(revenue + reservation_revenue));
+            stats->revenues[i] += price_per_night * (date_diff(range_end, range_begin) + 1);
         }
     }
 
@@ -153,32 +153,53 @@ int __q08_generate_statistics_foreach_reservation(void                *user_data
  * @param n          Number of query instances.
  * @param instances  Instances of the query 8.
  *
- * @return A `GHashTable` associating a ::q08_parsed_arguments_t to an integer revenue as a pointer.
+ * @return A pointer to a ::q08_statistical_data_t on success, or `NULL` on allocation failure.
  */
 void *__q08_generate_statistics(const database_t             *database,
                                 size_t                        n,
                                 const query_instance_t *const instances[n]) {
-    GPtrArray  *filter_data   = g_ptr_array_new();
-    GHashTable *hotel_revenue = g_hash_table_new(g_direct_hash, g_direct_equal);
 
-    for (size_t i = 0; i < n; ++i) {
-        const q08_parsed_arguments_t *argument_data =
-            query_instance_get_argument_data(instances[i]);
+    q08_statistical_data_t *const stats = malloc(sizeof(q08_statistical_data_t));
+    if (!stats)
+        return NULL;
+    stats->n = n;
 
-        /* TODO - find way to keep const */
-        g_hash_table_insert(hotel_revenue, (q08_parsed_arguments_t *) argument_data, 0);
-        g_ptr_array_add(filter_data, (q08_parsed_arguments_t *) argument_data);
+    stats->filters = malloc(n * sizeof(q08_parsed_arguments_t));
+    if (!stats->filters) {
+        free(stats);
+        return NULL;
     }
 
-    q08_foreach_reservation_data_t callback_data = {.filter_data   = filter_data,
-                                                    .hotel_revenue = hotel_revenue};
+    stats->revenues = malloc(n * sizeof(uint64_t));
+    if (!stats->revenues) {
+        free(stats->filters);
+        free(stats);
+        return NULL;
+    }
+
+    for (size_t i = 0; i < n; ++i) {
+        const q08_parsed_arguments_t *const argument_data =
+            query_instance_get_argument_data(instances[i]);
+
+        stats->filters[i]  = *argument_data;
+        stats->revenues[i] = 0;
+    }
 
     reservation_manager_iter(database_get_reservations(database),
                              __q08_generate_statistics_foreach_reservation,
-                             &callback_data);
+                             stats);
+    return stats;
+}
 
-    g_ptr_array_unref(filter_data);
-    return hotel_revenue;
+/**
+ * @brief Frees statistical data generated by ::__q08_generate_statistics.
+ * @param statistical_data Non-`NULL` value returned by ::__q08_generate_statistics.
+ */
+void __q08_free_statistics(void *statistical_data) {
+    q08_statistical_data_t *const stats = statistical_data;
+    free(stats->revenues);
+    free(stats->filters);
+    free(stats);
 }
 
 /**
@@ -186,12 +207,13 @@ void *__q08_generate_statistics(const database_t             *database,
  *
  * @param database   Database to get data from (not used, as all data is collected in
  *                   ::__q08_generate_statistics).
- * @param statistics Statistical data generated by ::__q08_generate_statistics.
+ * @param statistics Statistical data generated by ::__q08_generate_statistics (a pointer to a
+ *                   ::q08_statistical_data_t).
  * @param instance   Query instance to be executed.
  * @param output     Where to write the query's result to.
  *
- * @retval 0 Always succesl
- * @retval 1 Fatal failure (will only happen if a cosmic ray flips some bit in your memory).
+ * @retval 0 Success.
+ * @retval 1 Fatal failure (should, in principle, be unreachable).
  */
 int __q08_execute(const database_t       *database,
                   const void             *statistics,
@@ -199,19 +221,22 @@ int __q08_execute(const database_t       *database,
                   query_writer_t         *output) {
     (void) database;
 
-    GHashTable                   *hotel_revenue = (GHashTable *) statistics;
-    const q08_parsed_arguments_t *arguments     = query_instance_get_argument_data(instance);
+    const q08_statistical_data_t *const stats     = statistics;
+    const q08_parsed_arguments_t *const arguments = query_instance_get_argument_data(instance);
 
-    gpointer revenue_ptr;
-    if (g_hash_table_lookup_extended(hotel_revenue, arguments, NULL, &revenue_ptr)) {
-        uint64_t revenue = GPOINTER_TO_UINT(revenue_ptr);
-        query_writer_write_new_object(output);
-        query_writer_write_new_field(output, "revenue", "%" PRIu64, revenue);
-    } else {
-        fprintf(stderr, "Bad statistical data in query 3! This should not happen!\n");
-        return 1;
+    for (size_t i = 0; i < stats->n; ++i) {
+        if (stats->filters[i].hotel_id == arguments->hotel_id &&
+            stats->filters[i].begin_date == arguments->begin_date &&
+            stats->filters[i].end_date == arguments->end_date) {
+
+            query_writer_write_new_object(output);
+            query_writer_write_new_field(output, "revenue", "%" PRIu64, stats->revenues[i]);
+            return 0;
+        }
     }
-    return 0;
+
+    fprintf(stderr, "Bad statistical data in query 8! This should not happen!\n");
+    return 1;
 }
 
 query_type_t *q08_create(void) {
@@ -220,6 +245,6 @@ query_type_t *q08_create(void) {
                              __q08_clone_arguments,
                              free,
                              __q08_generate_statistics,
-                             (query_type_free_statistics_callback_t) g_hash_table_unref,
+                             __q08_free_statistics,
                              __q08_execute);
 }
